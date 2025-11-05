b83757e69e8e55c2a7ddea960202b6752dfb597a
AnandKarapadam <anandkv120@gmail.com>
2025-11-05 23:33:03 +0530

    feat: BST

---
+++ binarysearchtree.js
@@ -0,0 +1,147 @@
+class Node{
+    constructor(value){
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BST{
+    constructor(){
+        this.root = null;
+    }
+    insert(value){
+        this.root = this.insertRec(this.root,value)
+    }
+    insertRec(node,value){
+        if(node ===null){
+            return new Node(value);
+        }
+        if(value<node.value){
+            node.left = this.insertRec(node.left,value);
+        }else if(value>node.value){
+            node.right = this.insertRec(node.right,value);
+        }
+
+        return node;
+    }
+
+    preOrder(node){
+        if(!node){
+            return ;
+        }
+
+        console.log(node.value);
+        this.preOrder(node.left);
+        this.preOrder(node.right);
+    }
+    inOrder(node){
+        if(!node)return;
+
+        this.inOrder(node.left);
+        console.log(node.value);
+        this.inOrder(node.right);
+    }
+    postOrder(node){
+        if(!node)return;
+        this.postOrder(node.left);
+        this.postOrder(node.right);
+        console.log(node.value);        
+    }
+
+    levelOrder(){
+        if(!this.root)return;
+
+        let queue = [this.root];
+        while(queue.length){
+            let current = queue.shift();
+            console.log(current.value);
+            if(current.left)queue.push(current.left);
+            if(current.right)queue.push(current.right);
+        }
+    }
+    contains(node,value){
+        if(node===null)return false;
+        if(node.value===value)return true;
+
+        if(value<node.value){
+           return this.contains(node.left,value);
+        }else {
+            return this.contains(node.right,value);
+        }
+    }
+    height(node){
+        if(!node)return 0;
+
+        return 1+Math.max(this.height(node.left),this.height(node.right));
+    }
+    delete(value){
+        this.root = this.deleteRec(this.root,value);
+    }
+    deleteRec(node,value){
+       if(!node)return null;
+
+       if(value<node.value)node.left = this.deleteRec(node.left,value);
+       else if(value>node.value)node.right = this.deleteRec(node.right,value);
+       else{
+        if(!node.left)return node.right;
+        if(!node.right)return node.left;
+
+        node.value = this.minValue(node.right).value;
+        node.right = this.deleteRec(node.right,node.value);
+
+       }
+       return node;
+    }
+    minValue(node){
+        let current = node;
+
+        while(current.left){
+            current = current.left;
+        }
+        return current;
+    }
+
+
+
+    display(){
+        console.log(JSON.stringify(this.root,null,2));
+    }
+
+    findClosestValue(value){
+        return this.findClosest(this.root,value);
+    }
+    findClosest(node,target,closest = Infinity){
+        if(!node)return closest;
+
+        if(Math.abs(target-node.value)<Math.abs(target-closest)){
+            closest = node.value;
+        }
+
+        if(target<node.value){
+           return this.findClosest(node.left,target,closest);
+        }else if(target>node.value){
+           return this.findClosest(node.right,target,closest);
+        }else{
+            return node.value;
+        }
+    }
+    isBST(node = this.root,min = -Infinity,max=Infinity){
+        if(!node)return true;
+
+        if(node.value<=min||node.value>=max)return false;
+
+        return (this.isBST(node.left,min,node.value)&&this.isBST(node.right,node.value,max));
+    }
+}
+
+
+let bst = new BST();
+bst.insert(50);
+bst.insert(40);
+bst.insert(70);
+bst.insert(30);
+bst.insert(60);
+bst.insert(90);
+bst.display()
+
--- binarytree.js
+++ binarytree.js
@@ -1,87 +1,176 @@
-// //Tree is a non-linear data structure made up of nodes and connected widh edges. Each nodes can have zero or many child nodes.//
-// //Topics: Types of node, Traversal types, Implementation//
+//Tree is a non-linear data structure made up of nodes and connected widh edges. Each nodes can have zero or many child nodes.//
+//Topics: Types of node, Traversal types, Implementation//
 
 
-// //** BINARY TREE **//
+//** BINARY TREE **//
 
-// class Node{
-//     constructor(value){
-//         this.value = value;
-//         this.left = null;
-//         this.right = null;
-//     }
-// }
-// class BinaryTree{
-//     constructor(){
-//         this.root = null;
-//     }
-//     insertRoot(value){
-//         this.root = new Node(value)
-//     }
-//     insertLeft(parent,value){
-//         parent.left = new Node(value);
-//     }
-//     insertRight(parent,value){
-//         parent.right = new Node(value);
-//     }
+class Node{
+    constructor(value){
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+class BinaryTree{
+    constructor(){
+        this.root = null;
+    }
+    insertRoot(value){
+        this.root = new Node(value)
+    }
+    insertLeft(parent,value){
+        parent.left = new Node(value);
+    }
+    insertRight(parent,value){
+        parent.right = new Node(value);
+    }
 
-//     preOrder(node){
-//         if(node===null)return;
-//         console.log(node.value);
-//         this.preOrder(node.left);
-//         this.preOrder(node.right);
-//     }
+    preOrder(node){
+        if(node===null)return;
+        console.log(node.value);
+        this.preOrder(node.left);
+        this.preOrder(node.right);
+    }
 
-//     inOrder(node){
-//         if(node===null)return;
-//         this.inOrder(node.left);
-//         console.log(node.value);
-//         this.inOrder(node.right);
-//     }
+    inOrder(node){
+        if(node===null)return;
+        this.inOrder(node.left);
+        console.log(node.value);
+        this.inOrder(node.right);
+    }
 
-//     postOrder(node){
-//         if(node===null)return;
-//         this.postOrder(node.left);
-//         this.postOrder(node.right);
-//         console.log(node.value);
-//     }
+    postOrder(node){
+        if(node===null)return;
+        this.postOrder(node.left);
+        this.postOrder(node.right);
+        console.log(node.value);
+    }
 
-//     levelOrder(){
-//         if(this.root==null)return;
-//         let queue = [];
-//         queue.push(this.root);
+    levelOrder(){
+        if(this.root==null)return;
+        let queue = [];
+        queue.push(this.root);
 
-//         while(queue.length>0){
-//             let current = queue.shift();
-//             console.log(current.value); 
-//             if(current.left!==null)queue.push(current.left);
-//             if(current.right!==null)queue.push(current.right);
-//         }
-//     }
-// }
+        while(queue.length>0){
+            let current = queue.shift();
+            console.log(current.value); 
+            if(current.left!==null)queue.push(current.left);
+            if(current.right!==null)queue.push(current.right);
+        }
+    }
+    search(node,value){
+        if(node===null)return false;
+        if(node.value===value){
+            return true;
+        }
+
+        return this.search(node.left,value)||this.search(node.right,value);
+    }
+    height(node){
+        if(node===null)return 0;
+        let leftH = this.height(node.left);
+        let rightH = this.height(node.right);
+
+        return 1 + Math.max(leftH,rightH);
+    }
+    display(){
+        console.log(JSON.stringify(this.root,null,2));
+    }
+    countNodes(node){
+        if(node===null)return 0;
+        
+        return 1+this.countNodes(node.left)+this.countNodes(node.right);
+    }
+    countLeaves(node){
+        if(node===null)return 0;
+        if(node.left===null&&node.right===null)return 1;
+        return this.countLeaves(node.left)+this.countLeaves(node.right);
+    }
+
+    depth(node,value,level=0){
+        if(node==null)return -1;
+        if(node.value ===value)return level;
+        let leftDepth = this.depth(node.left,value,level+1);
+        if(leftDepth!==-1)return leftDepth;
+
+        return this.depth(node.right,value,level+1);
+    }
+
+    delete(value){
+        if(!this.root)return null;
+        
+        if(this.root.value === value && !this.root.left && !this.root.right){
+            this.root  = null;
+            return ;
+        }
+        
+        let queue  = [this.root];
+        let nodeToDelete = null;
+        let lastNode = null;
+        let parentOfLast = null;
 
 
-// let tree = new BinaryTree();
-// tree.insertRoot("A");
-// tree.insertLeft(tree.root,"B");
-// tree.insertRight(tree.root,"C");
-// tree.insertLeft(tree.root.left,"D");
-// tree.insertRight(tree.root.left,"E");
-// tree.insertLeft(tree.root.right,"F");
-// tree.insertRight(tree.root.right,"G")
+        while(queue.length){
+            let current = queue.shift();
+            if(current.value===value)nodeToDelete = current;
 
-// //** Pre-Order Traversal **//
-// console.log("Preorder Traversal:");
-// tree.preOrder(tree.root);
-// //** In-Order Traversal **//
-// console.log("Inorder Traversal:");
-// tree.inOrder(tree.root);
-// //** Post-Order Traversal **//
-// console.log("Postorder Traversal:");
-// tree.postOrder(tree.root);
+            if(current.left){
+                parentOfLast = current;
+                queue.push(current.left);
+            }
+            if(current.right){
+                parentOfLast = current;
+                queue.push(current.right);
+            }
+            lastNode = current;
+        }
+
+        if(!nodeToDelete){
+            console.log("Node not found");
+            return;
+        }
+
+        nodeToDelete.value = lastNode.value;
+        if(parentOfLast.right === lastNode) parentOfLast.right = null;
+        else if(parentOfLast.left === lastNode) parentOfLast.left = null;
+
+    }
+
+}
+
+
+let tree = new BinaryTree();
+tree.insertRoot("A");
+tree.insertLeft(tree.root,"B");
+tree.insertRight(tree.root,"C");
+tree.insertLeft(tree.root.left,"D");
+tree.insertRight(tree.root.left,"E");
+tree.insertLeft(tree.root.right,"F");
+tree.insertRight(tree.root.right,"G")
+
+
+//** Pre-Order Traversal **//
+console.log("Preorder Traversal:");
+tree.preOrder(tree.root);
+//** In-Order Traversal **//
+console.log("Inorder Traversal:");
+tree.inOrder(tree.root);
+//** Post-Order Traversal **//
+console.log("Postorder Traversal:");
+tree.postOrder(tree.root);
 
 // console.log(JSON.stringify(tree,null,2));
 
-// tree.levelOrder();
+tree.levelOrder();
+console.log(tree.height(tree.root));
+tree.display();
+console.log(tree.countNodes(tree.root));
+console.log(tree.search(tree.root,"K"));
+console.log(tree.countLeaves(tree.root));
+console.log(tree.depth(tree.root,"D"));
+
+
+
+
 
 
